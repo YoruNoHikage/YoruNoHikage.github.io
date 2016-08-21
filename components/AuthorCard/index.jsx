27da0e5e7e75bf1a86938f7d0ce1454989d26a33
Alexis Launay <yorunohikage@gmail.com>
2016-08-22 00:56:39 +0200

    Fix HTML semantic and remove useless float

--- components/AuthorCard/index.jsx
+++ components/AuthorCard/index.jsx
@@ -8,11 +8,11 @@ export default class AuthorCard extends Component {
 
     return (
       <div style={{display: 'flex'}}>
-        <img style={{margin: '20px'}} src={prefixLink(`/${avatar}`)} width='75' height='75' alt={username} className="floatLeft" />
-        <p style={{flex: '1'}}>
+        <img style={{margin: '20px'}} src={prefixLink(`/${avatar}`)} width='75' height='75' alt={username} />
+        <div style={{flex: '1'}}>
           <strong>{name}</strong> - @<a href={twitterLink} title={`${username} on Twitter`}>{username}</a>
-          <span dangerouslySetInnerHTML={{ __html: markdown().render(children).match(/<p>([^]+)<\/p>/)[1] }} />
-        </p>
+          <p style={{margin: '0'}} dangerouslySetInnerHTML={{ __html: markdown().render(children).match(/<p>([^]+)<\/p>/)[1] }} />
+        </div>
       </div>
     )
   }
