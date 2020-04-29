b79a6dab86c350ee6a01db9fee68e82b1e2a485d
Alexis Launay <yorunohikage@gmail.com>
2020-04-29 15:11:43 +0200

    Add multiple languages to articles

---
+++ articles/2018-07-30-making-a-twitter-emoji-map-with-yahoo-weather-api/index.ja.md
@@ -0,0 +1,22 @@
+---
+title: "Twitterで絵文字のおかげで日本の天気情報をお伝えします"
+date: "2018-07-30T07:18:45.164Z"
+categories:
+- Development
+---
+お久しぶりです！最近忙しくなったです！そして、リヨンに熱波が起こって、とても疲れさせました！
+ 
+先の日、友達は僕に絵文字で作った天気情報の全国の地図に入ったツイットを送りました。そのアイディアはいいと思ったので、アカウントをフォローしました。これは例えです：
+
+[![Emoji Weather Map France](emoji_weather_map_france.png)](https://twitter.com/TweeteoFrance)
+「今朝の天気」
+
+その後、色々なコミュニティマネージャーが自分のブランドでこんな地図を作ってジョークになりました。早くインターネット・ミームになって[Yahoo UKはそれをニュースにお伝えしました](https://uk.news.yahoo.com/weather-tweet-becomes-masterclass-french-161510338.html)。
+
+別の友達はブルターニュという僕たちのフランスの地元のバ－ションを作って送りました。それで、日本のバ－ションがありますかなと思って、なにも見つけませんでした。自分でこんな面白くて小さいプロジェクトを作って決まりました！
+
+![Emoji Weather Map Japan](emoji_weather_map_japan.png)
+ジャジャン！絵文字で日本の天気！
+
+サービスは自分で[@emoji_tenki](https://twitter.com/emoji_tenki)にツイットしています。気になったら、フォローして６時間ごとに天気をもらえます！:D
+未来にたぶん漢字でスペシャルバージョンを作って今考えてる。
--- pages/[...slug].jsx
+++ pages/[...slug].jsx
@@ -7,7 +7,14 @@ import moment from 'moment';
 import AuthorCard from '../components/AuthorCard';
 import avatar from '../images/yorunohikage.png';
 
-export default function Article({ title, date, content }) {
+export default function Article({
+  title,
+  path,
+  date,
+  content,
+  lang,
+  otherLangs,
+}) {
   const dateStyle = {
     textAlign: 'center',
     display: 'block',
@@ -16,36 +23,58 @@ export default function Article({ title, date, content }) {
   };
 
   return (
-    <div className="wrapper">
+    <div>
       <Head>
         <title>{title} - YoruNoHikage</title>
+
+        {otherLangs.length > 0 &&
+          [lang, ...otherLangs].map((l) => (
+            <link
+              rel="alternate"
+              hreflang={l}
+              href={'/' + (l !== 'en' ? l + '/' : '') + path}
+            />
+          ))}
       </Head>
 
-      <div>
-        <div>
-          <Link href="/">
-            <a className="gohome">All Articles</a>
-          </Link>
+      <div className="top-links">
+        <Link href="/">
+          <a className="gohome">All Articles</a>
+        </Link>
+
+        {otherLangs.length > 0 && (
+          <div className="lang-selector">
+            {otherLangs.map((l) => (
+              <Link
+                href="/[...slug]"
+                as={'/' + (l !== 'en' ? l + '/' : '') + path}
+                hreflang={l}
+              >
+                <a>{l}</a>
+              </Link>
+            ))}
+          </div>
+        )}
+      </div>
+
+      <div className="blog-single">
+        <div className="text" lang={lang}>
+          <h1>{title}</h1>
+          <em style={dateStyle}>
+            <time dateTime={date}>{moment(date).format('D MMMM YYYY')}</time>
+          </em>
+          <div dangerouslySetInnerHTML={{ __html: content }} />
         </div>
-        <div className="blog-single">
-          <div className="text">
-            <h1>{title}</h1>
-            <em style={dateStyle}>
-              <time dateTime={date}>{moment(date).format('D MMMM YYYY')}</time>
-            </em>
-            <div dangerouslySetInnerHTML={{ __html: content }} />
-          </div>
-          <hr />
-          <div className="footer">
-            <AuthorCard
-              name="Alexis Launay"
-              username="YoruNoHikage"
-              avatar={avatar}
-              twitterLink="https://twitter.com/YoruNoHikage"
-            >
-              Pop punk web developer indie game curious guy!
-            </AuthorCard>
-          </div>
+        <hr />
+        <div className="footer">
+          <AuthorCard
+            name="Alexis Launay"
+            username="YoruNoHikage"
+            avatar={avatar}
+            twitterLink="https://twitter.com/YoruNoHikage"
+          >
+            Pop punk web developer indie game curious guy!
+          </AuthorCard>
         </div>
       </div>
     </div>
@@ -55,17 +84,39 @@ export default function Article({ title, date, content }) {
 export async function getStaticProps({ params }) {
   const { slug } = params;
 
+  let lang = 'en';
+  let articleSlug = slug.join('-');
+  let path = slug.join('/');
+
+  if (slug[0].match(/^([a-z]{2})(-[A-Z]{2})?$/)) {
+    lang = slug[0];
+    articleSlug = slug.slice(1).join('-');
+    path = slug.slice(1).join('/');
+  }
+
+  const articlesPaths = glob.sync(`articles/${articleSlug}/index*.md`);
+  const otherLangs = articlesPaths
+    .map((path) =>
+      path.includes('index.md') ? 'en' : path.match(/.*index\.(.+)\.md/)[1]
+    )
+    .filter((otherLang) => otherLang !== lang);
+
   try {
-    const {content, data} = await import(`../articles/${slug.join('-')}/index.md`);
+    const { content, data } = await import(
+      `../articles/${articleSlug}/index${lang === 'en' ? '' : '.' + lang}.md`
+    );
 
     return {
       props: {
         ...data,
+        path,
         content,
+        lang,
+        otherLangs,
       },
     };
   } catch (err) {
-    console.log('Couldnt load', slug);
+    console.log('Couldnt load', articleSlug);
     return {
       props: {},
     };
@@ -73,14 +124,24 @@ export async function getStaticProps({ params }) {
 }
 
 export async function getStaticPaths() {
-  const articles = glob.sync('articles/**/*.md');
+  const articles = glob.sync('articles/**/index*.md');
 
-  const slugs = articles.map((file) => file.split('/')[1]);
-  const splittedSlugs = slugs.map((slug) => {
+  const splittedSlugs = articles.map((file) => {
+    const splittedPath = file.split('/');
+    const slug = splittedPath[1];
+    const filename = splittedPath[splittedPath.length - 1];
+
     // removing the full match
     const splits = slug.match(/(\d{4})-(\d{2})-(\d{2})-(.+)/);
 
-    return splits ? splits.slice(1) : [slug];
+    const arraySlug = splits ? splits.slice(1) : [slug];
+
+    // if it's not default lang
+    if (!filename.includes('index.md')) {
+      arraySlug.unshift(filename.match(/.*index\.(.+)\.md/)[1]);
+    }
+
+    return arraySlug;
   });
 
   return {
